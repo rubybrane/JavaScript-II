4cca425b552481820f6ab762efcab6c348838922
Vladimir Canro <vcanro@gmail.com>
2019-04-02 16:18:10 -0600

    WIP on all of the assigments

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,21 +1,45 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
-
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
-
+function name() {
+  const lastName = "Vladimir";
+    console.log(`My name is ${lastName}`);
+  function country() {
+    const city = "Venezuela";
+      console.log(`my origin country is ${city}`);
+  }
+  country();
+}
+name();
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
+
+    // Return a function that when invoked increments and returns a counter variable.
+    let count = 0;
+    return function () {
+      return ++count;
+  }
 };
+
+const newCounter = counter();
 // Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+console.log(newCounter()); // 1
+console.log(newCounter()); // 2
+console.log(newCounter()); // 3
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+    function counter () {
+      const increment =0;
+        return function (){
+          return ++increment; 
+        }
+        counter(); 
+    }
+    
 };
+console.log(counterFactory());
