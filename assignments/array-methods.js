323d2bb0f14a24e9aac8fcc9905b613b7d5d35c2
Vladimir Canro <vcanro@gmail.com>
2019-04-03 10:17:22 -0600

    working on arrays

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -90,7 +90,7 @@ console.log(`${ticketPriceTotal} is the total of ticket sales`);
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
-
+let shirtBySize = [];
 // Problem 2
 
 // Problem 3
