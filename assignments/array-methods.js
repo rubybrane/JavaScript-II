ea6a3597a2b1a0342256ea3dedd204eb5fd54bbc
Vladimir Canro <vcanro@gmail.com>
2019-04-02 18:09:46 -0600

    continuing improving the answers of the callbacks

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -78,8 +78,9 @@ let largeShirts = runners.filter(function(size){
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
-// The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+// The donations need to be tallied up and reported for tax purposes. 
+//Add up all the donations into a ticketPriceTotal array and log the result
+let ticketPriceTotal = ;
 
 
 console.log(ticketPriceTotal);
--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -29,19 +29,43 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+getLength(items, function(length){
+  console.log(length);
+});
 
-function last(arr, cb) {
+function lastItem(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length -1]);
 }
+lastItem(items, function(last){
+  console.log(last);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x,y,);
 }
+
+const add = function(num1, num2){
+  return num1 + num2;
+}
+console.log(sumNums(2,4,add));
+
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x,y);
 }
+
+const multiply = function(num1, num2){
+  return num1 * num2;
+}
+
+console.log(multiplyNums(3,9, multiply));
+
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
